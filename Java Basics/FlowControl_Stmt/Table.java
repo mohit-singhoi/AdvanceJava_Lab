3106c8881c8505db925fadfc343d4b2577dcf5f0
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-08 00:54:21 +0530

    initial commit

--- Java Basics/FlowControl_Stmt/Table.java
+++ Java Basics/FlowControl_Stmt/Table.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Table {
     public static void main(String[] args) {
-        System.out.println("Print Table form Range ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Table Number to Print Table :");
+        int n = sc.nextInt();
+        PrintTable(n);
+        sc.close();
+    }
+
+    public static void PrintTable(int n){
+        int table =0;
+        for(int i=1;i<=10;i++){
+            table = n*i;
+            System.out.println(n + " X " + i + " = " + table);
+        }
     }
 
     
