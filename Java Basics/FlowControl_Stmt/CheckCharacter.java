0d71ea94c210b20a91779194d5c23273493a9dbe
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-11 23:47:50 +0530

    initial commit

--- Java Basics/FlowControl_Stmt/CheckCharacter.java
+++ Java Basics/FlowControl_Stmt/CheckCharacter.java
@@ -3,13 +3,26 @@ import java.util.Scanner;
 public class CheckCharacter {
 
     public static void  CheckChar(char ch){
-        if (Character.isDigit(ch)) {
-            System.out.println("It is a Number.");
-        } 
-        else if (Character.isLetter(ch)) {
-            System.out.println("It is an Alphabet.");
-        } 
-        else {
+
+        // //1st 
+        // if (Character.isDigit(ch)) {
+        //     System.out.println("It is a Number.");
+        // } 
+        // else if (Character.isLetter(ch)) {
+        //     System.out.println("It is an Alphabet.");
+        // } 
+        // else {
+        //     System.out.println("It is a Special Character.");
+        // }
+
+        //2nd
+        if(ch>=48 && ch<=57){
+            System.out.println("It is Number");
+        }
+        else if(ch>=65 && ch<=90 || ch>=97 && ch<=122){
+            System.out.println("It is Character.");
+        }
+        else{
             System.out.println("It is a Special Character.");
         }
     }
