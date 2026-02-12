78d6eb430a1b35427130abc096685b02edeea972
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-12 23:55:32 +0530

    initial commit

---
+++ Java Basics/OOPS/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java OOPS Concept.");
+        System.out.println("There are Main 4 Pillors of OOPS Concept.");
+        System.out.println("1. Encapsulation\n2. Inheritance\n3. Polymorphism\n4. Abstraction");
+    }
+    
+}
