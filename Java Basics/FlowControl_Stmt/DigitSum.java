8f5a64dd387d39a3ce63fa1df7aa39ab5f07860a
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-13 23:54:56 +0530

    initail commit

---
+++ Java Basics/FlowControl_Stmt/DigitSum.java
@@ -0,0 +1,11 @@
+// WAP to print the sum of digit of a given Number.
+public class DigitSum {
+
+    public static int digitSum(int n){
+        while(num>0){
+            int r = n%10;
+            int sum = sum
+        }
+    }
+}
+
