379679e5e06992027616edf7418f6d1255fdda4c
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-14 00:00:39 +0530

    initial commit

--- Java Basics/FlowControl_Stmt/DigitSum.java
+++ Java Basics/FlowControl_Stmt/DigitSum.java
@@ -2,10 +2,24 @@
 public class DigitSum {
 
     public static int digitSum(int n){
-        while(num>0){
+        int sum =0;
+        while(n>0){
+            //int num = n;
+         
             int r = n%10;
-            int sum = sum
+            sum = sum+r;
+            n=n/10;
+            
         }
+        return sum;
+    }
+
+    //Main function
+    public static void main(String[] args) {
+        int n = 125;
+       int result = digitSum(n);
+       System.out.println("Sum of Digit : " +result);
+
     }
 }
 
